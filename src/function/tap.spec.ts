d3300cd5ebb08ee422399d5b4c949f01c2383467
Andre Luiz Rabello <andre.luiz_r@hotmail.com>
2020-07-05 18:06:50 +0100

    refactor(tap): types

--- src/function/tap.spec.ts
+++ src/function/tap.spec.ts
@@ -1,17 +1,18 @@
 import { tap } from './tap';
 
 describe('tap', () => {
-  it('should invoke the specified function and return the array itself', () => {
+  it('should invoke the specified function and return the argument itself', () => {
     let invoked = 0;
     const array = [1, 2, 3];
     const fn = () => invoked++;
 
+    // prettier-ignore
     const results = [
       tap(array, fn),
       tap (fn) (array),
     ];
 
-    results.forEach(result => {
+    results.forEach((result) => {
       expect(result).toBe(array);
     });
     expect(invoked).toBe(2);
--- src/function/tap.ts
+++ src/function/tap.ts
@@ -1,33 +1,36 @@
 import { infer } from './infer';
 
-/**
- * Invokes a function `fn` with `source` as argument, then returns the `source` itself.
- * @param source Argument to supply to `fn`.
- * @param fn Function to invoke with `source`.
- */
-export function tap<T>(source: T, fn: Tapper<T>): T;
-/**
- * Returns a function that
- * invokes a function `fn` with `source` as argument, then returns the `source` itself.
- * @param fn Function to invoke with `source`.
- */
-export function tap<T>(
-  fn: Tapper<T>
-): // prettier-ignore
-/**
- * Invokes a previously specified function `fn` with `source` as argument, then returns the `source` itself.
- * @param source Argument to supply to `fn`.
- */
-(source: Parameters<typeof fn>[0]) => T;
-export function tap() {
-  return inferred.apply(undefined, arguments);
+export const tap: Tap = infer(_tap);
+
+function _tap<T>(value: T, fn: Tapper<T>) {
+  fn(value);
+  return value;
 }
 
-type Tapper<T> = (source: T) => unknown;
+type Tapper<T> = (value: T) => unknown;
 
-const inferred = infer(
-  <T>(source: T, tapFn: Tapper<T>): T => {
-    tapFn(source);
-    return source;
-  }
-);
+interface Tap {
+  /**
+   * Calls `fn` with `value`, then returns `value`.
+   * @param value Value to provide as argument when calling `fn`.
+   * @param fn Function to call with `value`.
+   * @example
+   * const foo = tap('hi', console.log);
+   * // 'hi'
+   * console.log(foo);
+   * // 'hi'
+   */
+  <T>(value: T, fn: Tapper<T>): T;
+  /**
+   * Returns a function that
+   * calls `fn` with its first argument (`value`), then returns `value`.
+   * @param fn Function to call with `value`.
+   * @example
+   * const log = tap(console.log);
+   * const foo = log('hi');
+   * // 'hi'
+   * console.log(foo);
+   * // 'hi'
+   */
+  <T>(fn: Tapper<T>): (value: T) => T;
+}
